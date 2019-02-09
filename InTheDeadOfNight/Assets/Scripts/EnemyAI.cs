5a52bc0cf604d609b4f87c6d8c3a8e2042eaec17
zegregory <zegregory@knox.edu>
2019-02-09 15:11:04 -0600

    (!!!While loop in health Bar causes infinite loop!!!)
    Scripts for player Health.

--- InTheDeadOfNight/Assets/Scripts/EnemyAI.cs
+++ InTheDeadOfNight/Assets/Scripts/EnemyAI.cs
@@ -6,6 +6,10 @@ public class EnemyAI : MonoBehaviour
 {
     private const float CriticalArea = 5F;
     private GameObject playerGameObject;
+    public int Health = 100;
+    private float damRate= 1.0f;
+    private float canDam = 0.0f;
+
 
     void Start()
     {
@@ -32,8 +36,29 @@ public class EnemyAI : MonoBehaviour
         if (distanceToPlayer.sqrMagnitude <= CriticalArea)
             {
                 transform.position = Vector3.MoveTowards(new Vector3(transform.position.x, transform.position.y, transform.position.z), new Vector3(0, 0, 0), 0 * Time.deltaTime);
-            }       
+                
+        }       
     }
+
+
+    //Health
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+
+            //while (player.CurrHealth > 0)
+            //{
+                if (Time.time > canDam)
+                {
+                    player.CurrHealth--;
+                    canDam = Time.time + damRate;
+                }
+            //}
+        }
+    }
+
 
     void PowerUp2()
     {
---
+++ InTheDeadOfNight/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int StartHealth = 100;
+    public int CurrHealth;
+    public int Darkness = 0;
+
+    private void Awake()
+    {
+        CurrHealth = StartHealth;
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Death();
+        Debug.Log("Health: "+ CurrHealth);
+        Debug.Log(CurrHealth);
+    }
+
+    void Death()
+    {
+
+        if (CurrHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
